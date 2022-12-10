620916a96ecd007ac15d5bfb4cf20956f988bdfc
nursyahrina <nursyahrina17@gmail.com>
2022-12-10 15:15:43 +0700

    applied styled components

--- components/RestaurantItem.js
+++ components/RestaurantItem.js
@@ -1,17 +1,23 @@
-/* eslint-disable @next/next/no-img-element */
 import Link from 'next/link';
 import PropTypes from 'prop-types';
+import Card from './styled/Card';
+import Container from './styled/Container';
+import Image from './styled/Image';
  
 function RestaurantItem({ id, pictureId, name, description }) {
   return (
-    <div className="card">
-      <img src={`https://restaurant-api.dicoding.dev/images/small/${pictureId}`} alt={name} />
-      <div className="container">
+    <Card>
+       <Image
+        height="250px"
+        src={`https://restaurant-api.dicoding.dev/images/small/${pictureId}`}
+        alt={name} />
+ 
+      <Container>
         <h2><Link href={`/detail/${id}`}>{name}</Link></h2>
         <p>{description}</p>
         <br />
-      </div>
-    </div>
+      </Container>
+    </Card>
   );
 }
  
